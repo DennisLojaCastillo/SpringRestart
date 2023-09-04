6d9f2fb784cd58232424fc7dfa8c5b9ef6d0115b
DennisLojaCastillo <dennislojacastilllo@gmail.com>
2023-09-04 09:21:52 +0200

    Initial commit

---
+++ src/main/java/kea/dk/springrestart/SpringRestartApplication.java
@@ -0,0 +1,13 @@
+package kea.dk.springrestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/dk/springrestart/SpringRestartApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dk.springrestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
